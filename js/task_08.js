93fe49890d09a711d2239a1bd6139c0cbc0ba77c
AlexBelozertsev <kashim983@gmail.com>
2020-11-16 01:53:11 +0200

    +8_task

--- js/task_08.js
+++ js/task_08.js
@@ -1,9 +1,28 @@
 const input = document.querySelector('#controls > input');
-const container = document.querySelector('#controls')
 const renderBtn = document.querySelector('button[data-action="render"]');
 const destroyBtn = document.querySelector('button[data-action="destroy"]');
+const boxes = document.querySelector('#boxes');
 
-renderBtn.addEventListener('click', event => {
-    const item = document.createElement('div');
-    container.appendChild(item) += event.target.value;
-})
+destroyBtn.addEventListener('click', destroyBoxes);
+renderBtn.addEventListener('click', createBoxes);
+
+function createDivs(amount) {
+    let size = 30;
+    for (let i = 1; i < amount; i++) {
+        size += 10;
+        const createDiv = document.createElement('div');
+        createDiv.style.width = `${size}px`;
+        createDiv.style.height = `${size}px`;
+        createDiv.style.backgroundColor='#' + ( Math.random() * 0xFFFFF0 << 0 ).toString(16);
+        boxes.appendChild(createDiv);
+    }
+};
+function createBoxes() {
+    const value = Number(input.value);
+    createDivs(value);
+}
+
+function destroyBoxes() {
+    boxes.innerHTML = '';
+    input.value = '';
+};
