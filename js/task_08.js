08f13335b37ab9b63a730884dc406b37a7e890f7
AlexBelozertsev <kashim983@gmail.com>
2020-11-18 09:41:09 +0200

    переименован массив

--- js/task_08.js
+++ js/task_08.js
@@ -2,7 +2,7 @@ const input = document.querySelector('#controls > input');
 const renderBtn = document.querySelector('button[data-action="render"]');
 const destroyBtn = document.querySelector('button[data-action="destroy"]');
 const boxes = document.querySelector('#boxes');
-let div = [];
+let accumDivs = [];
 
 destroyBtn.addEventListener('click', destroyBoxes);
 renderBtn.addEventListener('click', createBoxes);
@@ -15,17 +15,16 @@ function createDivs(amount) {
         createDiv.style.width = `${size}px`;
         createDiv.style.height = `${size}px`;
         createDiv.style.backgroundColor = `#${Math.random().toString(16).substr(-6)}`;
-        div.push(createDiv);
+        accumDivs.push(createDiv);
     }
-    boxes.append(...div);
-
+    boxes.append(...accumDivs);
 };
 function createBoxes() {
     const value = Number(input.value);
     createDivs(value);
 };
 function destroyBoxes() {
-    div = [];
+    accumDivs = [];
     boxes.innerHTML = '';
     input.value = '';
 };
